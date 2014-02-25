6eebfe31b286c7ee73258e6fc92e50a9434bd3ee
happen-zhang <zhanghaipeng404@gmail.com>
2014-02-25 15:06:22 +0800

    添加Admin模块的配置

--- Admin/Common/Conf/config.php
+++ Admin/Common/Conf/config.php
@@ -1,4 +1,23 @@
 <?php
-return array(
-	//'配置项'=>'配置值'
+// $dbConfig = include('Global/Conf/db_config.php');
+
+$appConfig =  array(
+    // 调试页
+    'SHOW_PAGE_TRACE' =>true,
+
+    // 默认模块和Action
+    'MODULE_ALLOW_LIST' => array('Home'),
+    'DEFAULT_MODULE' => 'Home',
+
+    // 默认控制器
+    'DEFAULT_CONTROLLER' => 'Public',
+
+    // 分页列表数
+    'PAGINATION_NUM' => 20,
+
+    // 开启布局
+    'LAYOUT_ON' => true,
+    'LAYOUT_NAME' => 'Common/layout',
 );
+
+return array_merge($appConfig);
