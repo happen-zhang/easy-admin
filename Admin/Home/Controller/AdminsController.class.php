7af130f74efff3cdb7bc723646c1d81f27ffc54b
happen-zhang <zhanghaipeng404@gmail.com>
2014-03-01 19:39:27 +0800

    添加AdminsController及其页面视图

---
+++ Admin/Home/Controller/AdminsController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+
+/**
+ * AdminsController
+ * 管理员信息
+ */
+class AdminsController extends CommonController {
+    /**
+     * 修改密码
+     * @return
+     */
+    public function edit() {
+        $this->display();
+    }
+}
