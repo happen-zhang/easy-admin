20cea8a20eecf0602ea6872781ab6cb9723cd137
happen-zhang <zhanghaipeng404@gmail.com>
2014-03-01 19:40:53 +0800

    添加CacheController及其页面视图

---
+++ Admin/Home/Controller/CacheController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+
+/**
+ * CacheController
+ * 缓存管理
+ */
+class CacheController extends CommonController {
+    /**
+     * 缓存信息
+     * @return
+     */
+    public function index() {
+        $this->display();
+    }
+}
