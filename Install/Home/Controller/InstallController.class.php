ff8e5f6eff899052b02f162de09bada6e5de8346
happen-zhang <zhanghaipeng404@gmail.com>
2014-03-05 16:53:22 +0800

    完善Install表单

--- Install/Home/Controller/InstallController.class.php
+++ Install/Home/Controller/InstallController.class.php
@@ -1,11 +1,40 @@
 <?php
 namespace Home\Controller;
 
+/**
+ * InstallController
+ */
 class InstallController extends CommonController {
     public function index() {
         $this->display();
     }
 
+    /**
+     * 检查数据库连接
+     * @return
+     */
+    public function checkDbConnect() {
+        if (!isset($_POST['db']['password'])) {
+            return $this->ajaxReturn(false);
+        }
+
+        $host = $_POST['db']['host'] . ':' . $_POST['db']['port'];
+        $conn = mysql_connect($host,
+                              $_POST['db']['username'],
+                              $_POST['db']['password']);
+
+        if (!$conn) {
+            $this->ajaxReturn(false);
+        }
+
+        mysql_close($conn);
+        $this->ajaxReturn(true);
+    }
+
+    /**
+     * 创建数据
+     * @return
+     */
     public function create() {
         $this->display('installing');
     }
