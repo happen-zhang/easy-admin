243ab123e94650e4a81d8e62a007f507cfac9771
happen-zhang <zhanghaipeng404@gmail.com>
2014-03-15 16:14:57 +0800

    修改Admin/ Data/backup 数据表文件大小Bug

--- Admin/Common/Common/function.php
+++ Admin/Common/Common/function.php
@@ -8,14 +8,14 @@ include 'helper.php';
  * @return string
  */
 function bytes_format($bytes) {
-	// 单位
-    $uint = array(' B', ' KB', ' MB',
-    	              ' GB', ' TB', ' PB',
-    	              ' EB', ' ZB', ' YB');
+    // 单位
+    $unit = array(' B', ' KB', ' MB',
+                  ' GB', ' TB', ' PB',
+                  ' EB', ' ZB', ' YB');
 
     // bytes的对数
     $log_bytes = floor(log($bytes, 1024));
-    return round($bytes / pow(1024, log_bytes), 2) . $uint[$log_bytes];
+    return round($bytes / pow(1024, $log_bytes), 2) . $unit[$log_bytes];
 }
 
 /**
