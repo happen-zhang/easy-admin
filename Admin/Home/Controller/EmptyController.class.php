debc5a9b773598ca24618d46819ed083a014db85
happen-zhang <zhanghaipeng404@gmail.com>
2014-04-06 20:29:20 +0800

    修改Empty/ 实现自定义模型的CURD控制

--- Admin/Home/Controller/EmptyController.class.php
+++ Admin/Home/Controller/EmptyController.class.php
@@ -7,10 +7,71 @@ namespace Home\Controller;
  */
 class EmptyController extends CommonController {
     /**
-     * 重定向操作
+     * 模型数据列表首页
      * @return
      */
     public function index() {
-        $this->error('亲，您访问的页面不存在！');
+        $this->ensureExistContoller();
+
+        $this->display('Default/index');
+    }
+
+    /**
+     * 添加模型数据
+     * @return
+     */
+    public function add() {
+        $this->ensureExistContoller();
+
+        $this->display('Default/add');
+    }
+
+    /**
+     * 创建模型数据
+     * @return
+     */
+    public function create() {
+        $this->ensureExistContoller();
+
+        var_dump('craete');
+    }
+
+    /**
+     * 编辑模型数据
+     * @return
+     */
+    public function edit() {
+        $this->ensureExistContoller();
+
+        $this->display('Default/edit');
+    }
+
+    /**
+     * 更新模型数据
+     * @return
+     */
+    public function update() {
+        $this->ensureExistContoller();
+
+         var_dump('update');
+    }
+
+    /**
+     * 空操作
+     * @return
+     */
+    public function _empty() {
+        return $this->error('亲，您访问的页面不存在！');
+    }
+
+    /**
+     * 确保控制器对应的菜单存在
+     * @return
+     */
+    protected function ensureExistContoller() {
+    	$menu = fast_cache('model_menu', '', APP_PATH . '/Common/Conf/');
+        if (!array_key_exists(CONTROLLER_NAME, $menu)) {
+            return $this->_empty();
+        }
     }
 }
