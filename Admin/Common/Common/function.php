d6664c37e5b232da86af8d2994dc90f8688d1b8f
happen-zhang <zhanghaipeng404@gmail.com>
2014-05-05 10:21:23 +0800

    修改Admin/ Index/index 实现系统信息列表

--- Admin/Common/Common/function.php
+++ Admin/Common/Common/function.php
@@ -280,3 +280,53 @@ function trim_value($val) {
 
     return $val;
 }
+
+/**
+ * 得到操作系统信息
+ * @return string
+ */
+function get_system() {
+    $sys = $_SERVER['HTTP_USER_AGENT'];
+
+    if (stripos($sys, "NT 6.1")) {
+        $os = "Windows 7";
+    } else if (stripos($sys, "NT 6.0")) {
+        $os = "Windows Vista";
+    }  else if (stripos($sys, "NT 5.1")) {
+        $os = "Windows XP";
+    } else if (stripos($sys, "NT 5.2")) {
+        $os = "Windows Server 2003";
+    } else if (stripos($sys, "NT 5")) {
+        $os = "Windows 2000";
+    } else if (stripos($sys, "NT 4.9")) {
+        $os = "Windows ME";
+    } else if (stripos($sys, "NT 4")) {
+        $os = "Windows NT 4.0";
+    } else if (stripos($sys, "98")) {
+        $os = "Windows 98";
+    } else if (stripos($sys, "95")) {
+        $os = "Windows 95";
+    } else if (stripos($sys, "Mac")) {
+        $os = "Mac";
+    } else if (stripos($sys, "Linux")) {
+        $os = "Linux";
+    } else if (stripos($sys, "Unix")) {
+        $os = "Unix";
+    } else if (stripos($sys, "FreeBSD")) {
+        $os = "FreeBSD";
+    } else if (stripos($sys, "SunOS")) {
+        $os = "SunOS";
+    } else if (stripos($sys, "BeOS")) {
+        $os = "BeOS";
+    } else if (stripos($sys, "OS/2")) {
+        $os = "OS/2";
+    } else if (stripos($sys, "PC")) {
+        $os = "Macintosh";
+    } else if(stripos($sys, "AIX")) {
+        $os = "AIX";
+    } else {
+        $os = "未知操作系统";
+    }
+
+    return $os;
+}
--- Admin/Home/Controller/IndexController.class.php
+++ Admin/Home/Controller/IndexController.class.php
@@ -11,6 +11,36 @@ class IndexController extends CommonController {
      * @return
      */
     public function index(){
+        $gd = '不支持';
+        if (function_exists('gd_info')) {
+            $gd = gd_info();
+            $gd = $gd['GD Version'];
+        }
+
+        $hostport = $_SERVER['SERVER_NAME']
+                    ."($_SERVER[SERVER_ADDR]:$_SERVER[SERVER_PORT])";
+        $mysql = function_exists('mysql_close') ? mysql_get_client_info()
+                                                : '不支持';
+        $info = array(
+            'system' => get_system(),
+            'hostport' => $hostport,
+            'server' => $_SERVER['SERVER_SOFTWARE'],
+            'php_env' => php_sapi_name(),
+            'app_dir' => WEB_ROOT,
+            'mysql' => $mysql,
+            'gd' => $gd,
+            'upload_size' => ini_get('upload_max_filesize'),
+            'exec_time' => ini_get('max_execution_time') . '秒',
+            'disk_free' => round((@disk_free_space(".")/(1024 * 1024)),2).'M',
+            'server_time' => date("Y-n-j H:i:s"),
+            'beijing_time' => gmdate("Y-n-j H:i:s", time() + 8 * 3600),
+            'reg_gbl' => get_cfg_var("register_globals") == '1'? 'ON' : 'OFF',
+            'quotes_gpc' => (1 === get_magic_quotes_gpc()) ? 'YES' : 'NO',
+            'quotes_runtime' => (1===get_magic_quotes_runtime()) ?'YES' : 'NO',
+            'fopen' => ini_get('allow_url_fopen') ? '支持' : '不支持'
+        );
+
+        $this->assign('info', $info);
         $this->display();
     }
 }
