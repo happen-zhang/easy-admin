441cf75ae0e04594072366d87df5676d6187cd71
happen-zhang <zhanghaipeng404@gmail.com>
2014-05-14 15:24:02 +0800

    修改Install/ Data/database.sql 修改主键的注释为ID，添加toggleListShow节点

--- Install/Data/database.sql
+++ Install/Data/database.sql
@@ -2,7 +2,7 @@
 # 数据表：field 结构信息
 DROP TABLE IF EXISTS `ea_field`;
 CREATE TABLE `ea_field` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `model_id` int(11) NOT NULL COMMENT '所属模型id',
   `name` varchar(128) NOT NULL COMMENT '字段名称',
   `comment` varchar(32) NOT NULL COMMENT '字段注释',
@@ -30,7 +30,7 @@ CREATE TABLE `ea_field` (
 # 数据表：model 结构信息
 DROP TABLE IF EXISTS `ea_model`;
 CREATE TABLE `ea_model` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `name` varchar(32) NOT NULL COMMENT '模型名称',
   `tbl_name` varchar(32) NOT NULL COMMENT '数据表名称',
   `menu_name` varchar(32) NOT NULL COMMENT '菜单名称',
@@ -46,7 +46,7 @@ CREATE TABLE `ea_model` (
 # 数据表：input 结构信息
 DROP TABLE IF EXISTS `ea_input`;
 CREATE TABLE `ea_input` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `field_id` int(11) NOT NULL COMMENT '字段id',
   `is_show` tinyint(4) NOT NULL DEFAULT '0' COMMENT '表单域是否显示',
   `label` varchar(32) NOT NULL COMMENT '表单域标签',
@@ -68,7 +68,7 @@ CREATE TABLE `ea_input` (
 # 数据表：node 结构信息
 DROP TABLE IF EXISTS `ea_node`;
 CREATE TABLE `ea_node` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `pid` int(11) NOT NULL COMMENT '父节点id',
   `name` varchar(32) NOT NULL COMMENT '节点名称',
   `title` varchar(32) NOT NULL COMMENT '节点标题',
@@ -82,7 +82,7 @@ CREATE TABLE `ea_node` (
 # 数据库表：role 结构信息
 DROP TABLE IF EXISTS `ea_role`;
 CREATE TABLE `ea_role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `pid` int(11) NOT NULL COMMENT '父角色id',
   `name` varchar(32) NOT NULL COMMENT '角色名称',
   `description` text NOT NULL COMMENT '角色描述',
@@ -95,7 +95,7 @@ CREATE TABLE `ea_role` (
 # 数据库表：admin 结构信息
 DROP TABLE IF EXISTS `ea_admin`;
 CREATE TABLE `ea_admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `role_id` int(11) NOT NULL COMMENT '所属角色id',
   `email` varchar(64) NOT NULL COMMENT '登录邮箱',
   `password` varchar(32) NOT NULL COMMENT '登录密码',
@@ -170,4 +170,4 @@ INSERT INTO `ea_role` (`id`, `pid`, `name`, `description`, `status`, `created_at
 
 # 节点数据
 
-INSERT INTO `ea_node` (`id`, `pid`, `name`, `title`, `level`, `status`, `created_at`, `updated_at`) VALUES(1, 0, 'Admin', '后台管理', 1, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(2, 1, 'Index', '首页', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(3, 2, 'index', '查看系统信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(4, 2, 'editPassword', '修改密码页面', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(5, 2, 'updatePassword', '更新密码', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(6, 2, 'siteEdit', '编辑站点信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(7, 2, 'siteUpdate', '更新站点信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(8, 1, 'Cache', '缓存管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(9, 8, 'index', '缓存文件列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(10, 8, 'delete', '删除缓存文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(11, 1, 'Admins', '管理员权限', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(12, 11, 'index', '查看管理员列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(13, 11, 'add', '添加管理员', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(14, 11, 'create', '创建管理员', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(15, 11, 'edit', '编辑管理员信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(16, 11, 'update', '更新管理员信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(17, 1, 'Roles', '角色管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(18, 17, 'index', '查看角色列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(19, 17, 'add', '添加角色', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(20, 17, 'create', '创建角色', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(21, 17, 'edit', '编辑角色信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(22, 17, 'update', '更新角色信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(23, 17, 'assignAccess', '编辑角色权限', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(24, 17, 'doAssignAccess', '更新角色权限', 3, 1, 0, 0),(25, 1, 'Nodes', '节点管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(26, 25, 'index', '查看节点信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(27, 25, 'toggleStatus', '修改节点状态', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(28, 1, 'Models', '模型管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(29, 28, 'index', '查看模型列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(30, 28, 'show', '查看模型信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(31, 28, 'add', '添加模型', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(32, 28, 'create', '创建模型', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(33, 28, 'edit', '编辑模型信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(34, 28, 'update', '更新模型信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(35, 28, 'delete', '删除模型', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(36, 28, 'checkModelName', '检查模型名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(37, 28, 'checkTblName', '检查数据表名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(38, 28, 'checkMenuName', '检查菜单名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(39, 1, 'Fields', '字段管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(40, 39, 'index', '查看字段信息列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(41, 39, 'add', '添加字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(42, 39, 'create', '创建字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(43, 39, 'edit', '编辑字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(44, 39, 'update', '更新字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(45, 39, 'delete', '删除字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(46, 39, 'checkFieldName', '检查字段名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(47, 39, 'checkFieldLabel', '检查字段标签', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(48, 1, 'Data', '数据管理', 2, 1, 1, UNIX_TIMESTAMP()),(49, 48, 'backup', '查看数据备份列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(50, 48, 'doBackup', '处理数据备份', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(51, 48, 'restore', '查看SQL文件列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(52, 48, 'doRestore', '恢复SQL文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(53, 48, 'deleteSqlFiles', '删除SQL文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(54, 48, 'zipSqlFiles', '打包SQL文件为ZIP', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(55, 48, 'zipList', '查看ZIP文件列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(56, 48, 'unzipFiles', '解压ZIP文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(57, 48, 'deleteZipFiles', '删除ZIP文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(58, 48, 'downloadFile', '下载SQL、ZIP文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(59, 48, 'optimize', '查看可优化数据表列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(60, 48, 'doOptimize', '优化数据表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+INSERT INTO `ea_node` (`id`, `pid`, `name`, `title`, `level`, `status`, `created_at`, `updated_at`) VALUES(1, 0, 'Admin', '后台管理', 1, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(2, 1, 'Index', '首页', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(3, 2, 'index', '查看系统信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(4, 2, 'editPassword', '修改密码页面', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(5, 2, 'updatePassword', '更新密码', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(6, 2, 'siteEdit', '编辑站点信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(7, 2, 'siteUpdate', '更新站点信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(8, 1, 'Cache', '缓存管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(9, 8, 'index', '缓存文件列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(10, 8, 'delete', '删除缓存文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(11, 1, 'Admins', '管理员权限', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(12, 11, 'index', '查看管理员列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(13, 11, 'add', '添加管理员', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(14, 11, 'create', '创建管理员', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(15, 11, 'edit', '编辑管理员信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(16, 11, 'update', '更新管理员信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(17, 1, 'Roles', '角色管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(18, 17, 'index', '查看角色列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(19, 17, 'add', '添加角色', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(20, 17, 'create', '创建角色', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(21, 17, 'edit', '编辑角色信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(22, 17, 'update', '更新角色信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(23, 17, 'assignAccess', '编辑角色权限', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(24, 17, 'doAssignAccess', '更新角色权限', 3, 1, 0, 0),(25, 1, 'Nodes', '节点管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(26, 25, 'index', '查看节点信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(27, 25, 'toggleStatus', '修改节点状态', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(28, 1, 'Models', '模型管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(29, 28, 'index', '查看模型列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(30, 28, 'show', '查看模型信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(31, 28, 'add', '添加模型', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(32, 28, 'create', '创建模型', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(33, 28, 'edit', '编辑模型信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(34, 28, 'update', '更新模型信息', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(35, 28, 'delete', '删除模型', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(36, 28, 'checkModelName', '检查模型名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(37, 28, 'checkTblName', '检查数据表名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(38, 28, 'checkMenuName', '检查菜单名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(39, 1, 'Fields', '字段管理', 2, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(40, 39, 'index', '查看字段信息列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(41, 39, 'add', '添加字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(42, 39, 'create', '创建字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(43, 39, 'edit', '编辑字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(44, 39, 'update', '更新字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(45, 39, 'delete', '删除字段', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(46, 39, 'checkFieldName', '检查字段名称', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(47, 39, 'checkFieldLabel', '检查字段标签', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(48, 1, 'Data', '数据管理', 2, 1, 1, UNIX_TIMESTAMP()),(49, 48, 'backup', '查看数据备份列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(50, 48, 'doBackup', '处理数据备份', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(51, 48, 'restore', '查看SQL文件列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(52, 48, 'doRestore', '恢复SQL文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(53, 48, 'deleteSqlFiles', '删除SQL文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(54, 48, 'zipSqlFiles', '打包SQL文件为ZIP', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(55, 48, 'zipList', '查看ZIP文件列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(56, 48, 'unzipFiles', '解压ZIP文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(57, 48, 'deleteZipFiles', '删除ZIP文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(58, 48, 'downloadFile', '下载SQL、ZIP文件', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(59, 48, 'optimize', '查看可优化数据表列表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(60, 48, 'doOptimize', '优化数据表', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),(61, 39, 'toggleListShow', '切换系统字段在数据列表中的显示', 3, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
